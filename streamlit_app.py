5b05a978bf61768a94e36f4d30453cda3425fdf6
FaisalSuwayyid <145586543+FaisalSuwayyid@users.noreply.github.com>
2023-10-16 17:46:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,93 +6,29 @@ import plotly.express as px
 import numpy as np
 
 
+
+
 ######################################################################
-
-# pre-defined functions
-
-##### relative hist plots ####
-
-def relative_histplot(df, x):
-    s = df.columns.tolist()
-    s.remove('target')
-
-    
-
-    # print(x)
-    y = 'target'
-
-    x1 = df[x].unique().tolist()
-    y1 = df[y].unique().tolist()
-
-    a = df[[x, y]].value_counts()
-
-    A = np.zeros((len(x1), len(y1)))
-
-    for i, labeli in zip(range(len(x1)), x1):
-        for j, labelj in zip(range(len(y1)), y1):
-            if labelj in a[labeli].index:
-                A[i][j] = a[labeli][labelj]
-
-
-    # memo of sample number
-    snum = np.sum(A, axis=1)
-
-
-    # normalization
-    for i in range(A.shape[0]):
-        A[i] = A[i]/snum[i]*100.
-
-    A = A.T
-    fig=plt.figure(figsize=(6,4), dpi=250)
-    ax=fig.add_subplot(111)
-
-
-    # stack bars
-    for i in range(A.shape[0]):
-            plt.bar(x1, A[i], bottom = 0.6*i + np.sum(A[:i], axis = 0), label=y1[i], width=0.8,  linewidth=0.1)
-
-
-    # add text annotation corresponding to the percentage of each data.
-
-    for i in range(A.shape[0]):
-        if i==0:
-            for xpos, ypos, yval in zip(x1, A[i]/2, A[i]):
-                plt.text(xpos, ypos, "N=%.2f"%yval, ha="center", va="center")
-        else :
-            for xpos, ypos, yval in zip(x1, np.sum(A[:i], axis=0) + A[i]/2, A[i]):
-                plt.text(xpos, ypos, "N=%.2f"%yval, ha="center", va="center")
-    # for xpos, ypos, yval in zip(x1, 0.6 + np.sum(A[:], axis=0), np.sum(A[:], axis=0)):
-    #     plt.text(xpos, ypos, "N=%.2f"%yval, ha="center", va="bottom") 
-
-    # plt.ylim(0,110)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    # ax.spines['bottom'].set_color('#DDDDDD')
-
-    plt.xticks(rotation=45)
-    plt.xlabel(x)
-    plt.ylabel('relative percentage')
-    plt.title('relative percentage of poisonous and edible in each category')
-    plt.legend(bbox_to_anchor=(1.01,0.5), loc='center left')
-    # plt.show()
-    st.pyplot(fig)
-
-
-
-
-##############################
-
-# st.plotly_chart(figure_or_data, use_container_width=False, sharing="streamlit", theme="streamlit", **kwargs)
-# st.pyplot(fig=None, clear_figure=None, use_container_width=True, **kwargs)
-#####################################################################
-
-
 
 
 
 df = pd.read_csv('https://raw.githubusercontent.com/FaisalSuwayyid/CMSE830/main/Mushroom.csv', index_col=0)
-
+df = df[['target',
+ 'cap-shape',
+ 'cap-surface',
+ 'cap-color',
+ 'odor',
+ 'gill-attachment', #
+ 'gill-spacing', #
+ 'gill-color',
+ 'stalk-surface-above-ring',
+ 'stalk-surface-below-ring',
+ 'stalk-color-above-ring',
+ 'stalk-color-below-ring',
+ 'veil-color', #
+ 'spore-print-color',
+ 'population',
+ 'habitat']]
 
 # df = pd.read_csv('/Users/faisalsuwayyid/Desktop/Projects/CMSE_Midterm/The project/7_Mushroom/Mushroom.csv')
 # df = df.iloc[:,1:]
@@ -102,7 +38,20 @@ st.set_page_config(page_title = 'Classification of Mushrooms to Edible and Poiso
                    layout='wide')
 st.title('Classification of Mushrooms to Edible and Poisonous')
 
-
+st.markdown('#')
+st.markdown('#')
+st.markdown('##### In this application, we have a dataset \
+            containing features characterizing mushrooms,\
+            and the goal is to classify mushrooms to poisonous and edible. \
+            There are 23 features characterizing 8124 different mushrooms. Therefore, we \
+            believe we can reduce the number of features without affecting the data. First, \
+            when we drop the target column, we find\
+            that there is no two samples with the same features. Therefore, we can always tell if \
+            a mushroom is poisonous or not with absolute certainty. \
+            Carrying out the same process with every feature, we are able to reduce the number of \
+            features to 15.')
+st.markdown('#')
+st.markdown('#')
 
 
 
@@ -117,33 +66,45 @@ with st.expander('Data Preview'):
     #                  'Target': st.column_config.NumberColumn(format='%d')
     #              })
 ###############################
-
+st.markdown('#')
+st.markdown('#')
+st.markdown('##### In the below plot, we notice that we have poisonous \
+             mushrooms almost as much as edible ones.')
+st.markdown('#')
+st.markdown('#')
 ####### Data Validation #########
 col1, col2, col3 = st.columns(3)
 with col2:
     st.markdown('# Size of data')
-fig = plt.figure(figsize=(6, 4))
-ax=fig.add_subplot(111)
-sns.histplot(df, x='target', hue='target', ax=ax)
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
 
-# st.pyplot(fig1, theme="streamlit", use_container_width=True)
-st.pyplot(fig)
-# st.write(fig1)
+option_data_validation = st.selectbox(
+   "Select a feature to view its histogram",
+   tuple([None, 'percent']),
+   index=0,
+   placeholder="Select contact method...",
+)
+fig = px.histogram(df, x="target", text_auto=True,
+                    histnorm = option_data_validation, histfunc = 'count')
+st.plotly_chart(fig, use_container_width=True)
+
+
+
+
 ###############################
 
-
+st.markdown('##### We investigate the histogram of each \
+            feature to understand if there is any deterministic \
+            feature that characterizes mushrooms to edible or poisonous \
+            and how mushrooms are distributed over classes of the feature selected.')
 ############ Histogram count ###############
 col1, col2, col3 = st.columns([0.2, 0.7, 0.1])
 with col2:
-    st.markdown('# Distribution of classes of features')
+    st.markdown('# The percentage of classes in features')
 fig = plt.figure(figsize=(6, 4))
 s = df.columns.tolist()
 s = [ele for ele in s if ele != 'target']
 # s = s.remove('target')
-option = st.selectbox(
+option_data_histogram = st.selectbox(
    "Select a feature to view its histogram",
    tuple(s),
    index=0,
@@ -151,27 +112,126 @@ option = st.selectbox(
 )
 
 
-x1 = df[option].unique().tolist()
+x1 = df[option_data_histogram].unique().tolist()
 fig2=plt.figure(figsize=(6,4), dpi=250)
 ax=fig2.add_subplot(111)
 
-sns.histplot(data=df, x=option, hue='target', multiple='stack', stat='percent', ax=ax)
+sns.histplot(data=df, x=option_data_histogram,
+              hue='target', multiple='stack', stat='percent', ax=ax)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
 # ax.spines['bottom'].set_color('#DDDDDD')
 plt.xticks(rotation=45)
-plt.title(f'Percentage of classes in {option}')
+plt.title(f'Percentage of classes in {option_data_histogram}')
 st.pyplot(fig2)
+
+
 
 ###############################
 
 #### Relative Histogram #####
 st.markdown('#')
-relative_histplot(df, option)
+st.markdown('##### The below plot shows the percentage of both \
+            poisonous and edible mushrooms in the same class\
+            in the selected feature.')
+# relative_histplot(df, option)
 
+fig = px.histogram(df, x=option_data_histogram,
+                    text_auto=True, color = 'target', barnorm = 'percent')
+st.plotly_chart(fig, use_container_width=True)
 
 ##############################
+st.markdown('#')
+st.markdown('#')
+st.markdown('##### Going over the features, we observe that the best\
+            classifying features are odor, \
+            spore-print-color and veil-color.')
+st.markdown('#')
+st.markdown('#')
+st.markdown('##### Here, we investigate the distribution and probability of selected pair of features. \
+            The below table shows the probablities of the pair of classes of the selected two features.\
+            The left table shows the probability of edible mushrooms over the pair of classes of\
+            the pair of selected features. The right table shows the ones for the poisonous mushrooms.\
+            Zero percentage corresponding to a pair of classes in the left table \
+            implies that all mushrooms with such pair of classes are \
+            poisonous, if any, and vice versa.')
+st.markdown('#')
+st.markdown('#')
+
+s = df.columns.tolist()
+s = [ele for ele in s if ele != 'target']
+
+option_feature1 = st.selectbox(
+   "Feature 1",
+   tuple(s),
+   index=0,
+   placeholder="Select contact method...",
+)
+s = [ele for ele in s if ele != option_feature1]
+option_feature2 = st.selectbox(
+   "Feature 2",
+   tuple(s),
+   index=0,
+   placeholder="Select contact method...",
+)
+
+df_p = df[df['target']=='poisonous']
+df_e = df[df['target']=='edible']
+
+feature1 = option_feature1
+feature2 = option_feature2
+
+x = df[[feature1, feature2]].value_counts()
+feature1_classes = df[feature1].unique()
+feature2_classes = df[feature2].unique()
+
+
+
+a = np.zeros((len(feature1_classes), len(feature2_classes)))
+# df[[feature1, feature2]].value_counts()
+for i, labeli in zip(range(len(feature1_classes)), feature1_classes):
+    for j, labelj in zip(range(len(feature2_classes)), feature2_classes):
+        if labelj in x[labeli].index:
+            a[i][j] = x[labeli][labelj]
+
+a = a/df.shape[0]*100
+A = pd.DataFrame(a, columns = feature2_classes, index = feature1_classes)
+
+a_e = np.zeros_like(a)
+
+x_e = df_e[[feature1, feature2]].value_counts()
+feature1_classes_e = df_e[feature1].unique()
+
+for i, labeli in zip(range(len(feature1_classes)), feature1_classes):
+    for j, labelj in zip(range(len(feature2_classes)), feature2_classes):
+        if labeli in feature1_classes_e:
+            if labelj in x_e[labeli].index:
+                a_e[i][j] = x_e[labeli][labelj]
+            
+a_e = a_e/df.shape[0]*100
+A_e = pd.DataFrame(a_e, columns = feature2_classes, index = feature1_classes)
+
+a_p = a - a_e
+A_p = A - A_e
+
+with st.expander('Probability Table of the two selected features'):
+    # st.write(df)
+    st.dataframe(A)
+    # st.dataframe(df,
+    #              column_config={
+    #                  'Target': st.column_config.NumberColumn(format='%d')
+    #              })
+
+col1, col2 = st.columns([0.5, 0.5])
+with col1:
+    with st.expander('Probability table of the edible ones'):
+    # st.write(df)
+        st.dataframe(A_e)
+with col2:
+    with st.expander('Probability table of the poisonous ones'):
+    # st.write(df)
+        st.dataframe(A_p)
 
 
 
@@ -181,41 +241,67 @@ relative_histplot(df, option)
 
 
 
-
-
-# ######### Sunburst plot ############
-
+# ######### Sunburst1 plot ############
+st.markdown('#')
+st.markdown('#')
+st.markdown('##### We use a sunburst plot to display a 3d plot to help in understanding \
+            how three features can classify mushrooms.')
+st.markdown('#')
+st.markdown('#')
 col1, col2, col3 = st.columns(3)
 with col1:
     s = df.columns.tolist()
     s = [ele for ele in s if ele != 'target']
 
-    option5 = st.selectbox(
+    option_first_sunburst1_feature = st.selectbox(
     "Select first feature for sunburst plot",
     tuple(s),
     index=0,
     placeholder="Select contact method...",
     )
-    s = [ele for ele in s if ele != 'target' and ele != option5]
-    option6 = st.selectbox(
+    s = [ele for ele in s if ele != 'target' and \
+          ele != option_first_sunburst1_feature]
+    option_second_sunburst1_feature = st.selectbox(
     "Select second feature for sunburst plot",
     tuple(s),
     index=0,
     placeholder="Select contact method...",
     )
-    s = [ele for ele in s if ele != 'target' and ele != option5 and ele != option6]
-    option7 = st.selectbox(
+    s = [ele for ele in s if ele != 'target' and \
+          ele != option_first_sunburst1_feature and ele != option_second_sunburst1_feature]
+    option_third_sunburst1_feature = st.selectbox(
     "Select third feature for sunburst plot",
     tuple(s),
     index=0,
     placeholder="Select contact method...",
     )
-    fig = px.sunburst(df, path=[option5,
-    option6,
-    option7,
+    fig = px.sunburst(df, path=[option_first_sunburst1_feature,
+    option_second_sunburst1_feature,
+    option_third_sunburst1_feature,
     'target'])
 with col2:
     st.plotly_chart(fig)
+
+# hip.Experiment.from_dataframe(df).display().to_streamlit()
+
+#############################################
+
+
+
+# ######### Sunburst2 plot ############
+
+# df2 = df[df['odor']=='none']
+
+# st.markdown('#')
+# st.markdown('#')
+# st.markdown('##### After trying several combination, we found that it is possible \
+#             to classify mushrooms to edible and poisonous using only seven features.')
+# st.markdown('#')
+# st.markdown('#')
+
+# fig = px.sunburst(df, path = df.columns.tolist())
+# with col2:
+#     st.plotly_chart(fig)
 
 # hip.Experiment.from_dataframe(df).display().to_streamlit()
 
@@ -261,13 +347,26 @@ df_specific = df[df[option2]==option3]
 df_specific = df_specific.drop(columns=[option2])
 
 
-####### Data Validation #########
-fig = plt.figure(figsize=(6, 4))
-sns.histplot(data =df_specific, x='target', hue='target')
+# ####### Data Validation #########
+# fig = plt.figure(figsize=(6, 4))
+# sns.histplot(data =df_specific, x='target', hue='target')
 
-# st.pyplot(fig1, theme="streamlit", use_container_width=True)
-st.pyplot(fig)
-# st.write(fig1)
+# # st.pyplot(fig1, theme="streamlit", use_container_width=True)
+# st.pyplot(fig)
+# # st.write(fig1)
+
+
+option12 = st.selectbox(
+   "Select histogram type",
+   tuple([None, 'percent']),
+   index=0,
+   placeholder="Select contact method...",
+)
+fig = px.histogram(df_specific, x="target", text_auto=True, histnorm = option12, histfunc = 'count')
+st.plotly_chart(fig, use_container_width=True)
+
+
+
 ###############################
 s = df_specific.columns.tolist()
 s = [ele for ele in s if ele != 'target' and option2]
@@ -291,7 +390,8 @@ plt.xticks(rotation=45)
 st.pyplot(fig2)
 
 
+# relative_histplot(df_specific, option4)
 
-relative_histplot(df_specific, option4)
-
+fig = px.histogram(df_specific, x=option4, text_auto=True, color = 'target', barnorm = 'percent')
+st.plotly_chart(fig, use_container_width=True)
 #########################################
